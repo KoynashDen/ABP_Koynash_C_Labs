3cab04bb9f566c70439c1a9c0df2f9c111b56921
Denys <koynashden07@gmail.com>
2024-09-18 01:05:12 +0300

    test

---
+++ Lab_1/Lab_1.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Denys on 18.09.2024.
+//
+
+#include "Lab_1.h"
---
+++ Lab_1/Lab_1.h
@@ -0,0 +1,16 @@
+//
+// Created by Denys on 18.09.2024.
+//
+
+#ifndef LAB_1_H
+#define LAB_1_H
+
+
+
+class Lab_1 {
+
+};
+
+
+
+#endif //LAB_1_H
