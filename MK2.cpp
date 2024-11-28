a366c5125c6c92238c7a6721648354bb35402357
Denys <koynashden07@gmail.com>
2024-11-28 21:13:00 +0200

    mk2

--- MK2.cpp
+++ MK2.cpp
@@ -6,30 +6,12 @@
 
 //1
 // #include <iostream>
-// #include <cmath>
 // using namespace std;
 // bool collinearity(int x1, int y1, int x2, int y2)
 // {
-//     if ((x1 == 0 && x2 == 0) || (y1 == 0 && y2 == 0)){
-//
-//         return true;
-//
-//
-//     }else if((x2!=0 && y2!=0) && (fabs(x1)>=fabs(x2))){
-//         if(x1/x2 == y1/y2){
-//             return true;
-//         }
-//     }else if(x1!=0 && y1!=0 && (fabs(x1)<=fabs(x2))){
-//         if(x2/x1 == y2/y1){
-//             return true;
-//         }
-//     }else if(x1 == 0 && x2 == 0){
-//         return true;
-//     }else if(y1 == 0 && y2 == 0){
-//         return true;
+//     if(x1*y2 == x2*y1){
+//     return true;
 //     }
-//
-//
 //     return false;
 // }
 
